393cabf7ed0fb00ff9a9eb14219c487cf8ac3eb3
Niklas Gögge <n.goeggi@gmail.com>
2019-03-14 22:18:08 +0100

    Add package

---
+++ README.md
@@ -0,0 +1,65 @@
+# Firebase security rules testing
+
+There is currently no official testing framework for firebase storage security rules.
+
+There is how ever the [Firebase Rules API](https://developers.google.com/apis-explorer/?hl=en_US#search/firebaserules/firebaserules/v1/) which does provide unit testing functionality.
+The simulator in the firebase console makes use of this api.
+
+This repository is an attempt at a client for this api and since the api works for both **Firestore** and **Firebase Storage** you will be able to test both types of security rules with this client.
+
+I also opened a feature request with firebase, so maybe they will realease something like this pretty soon with official support.
+
+## Install
+```sh
+npm i firebase-rules-testing
+```
+
+## Setup
+In order for this package to work you will have to set the `FIREBASE_TOKEN` environment variable.
+You can obtain the token with:
+```sh
+firebase login:ci
+```
+
+## Examples
+
+## 
+```javascript
+const { RuleTestSuite, validateRuleSuite } = require('firebase-rules-testing');
+
+// Initialise the rule test suite
+var storageRules = new RuleTestSuite({
+	rulePath: 'storage.rules', // path to your rules file
+	project: '<project-name>', // your project name
+	description: 'storage rules' // optional for logging of results
+});
+
+// add a test case
+storageRules.test('<description>'/* description for logging */, {
+	path: '/b/<bucket-name>/o/path/to/resource',
+	method: 'get',
+	auth: {
+		uid: '...',
+		token: {
+			'...': '...'
+		}
+	}
+}).shouldSucceed(); // this test case should succeed
+
+// ad another test case
+storageRules.test('<description>'/* description for logging */, {
+	path: '/b/<bucket-name>/o/path/to/resource',
+	method: 'create'
+}).shouldFail(); // this test case should fail
+
+// validate the tests
+validateRuleSuite(storageRules, { logging: true });
+
+```
+
+## Test case options
+
+The options for the test cases ("as far as i can tell") are the ones listed in the documentation for [firestore request](https://firebase.google.com/docs/reference/rules/rules.firestore.Request), [firestore resource](https://firebase.google.com/docs/reference/rules/rules.firestore.Resource), [storage request](https://firebase.google.com/docs/reference/security/storage/#request), [storage resource](https://firebase.google.com/docs/reference/security/storage/#resource)
+
+## Contributing
+Every contribution welcome just open a issue or pr...
---
+++ index.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+var clc = require("cli-color");
+const api = require('firebase-tools/lib/api');
+const requireAuth = require('firebase-tools/lib/requireAuth');
+
+function log(logger, msg, shouldLog) {
+	if (shouldLog) logger(msg);
+}
+
+function buildRequest(suite) {
+	return {
+		auth: true,
+		origin: api.rulesOrigin,
+		data: {
+			source: {
+				files: {
+					name: suite.ruleName,
+					content: suite.ruleContent,
+				}
+			},
+			testSuite: {
+				testCases: suite.testCases.map(testCase => {
+					return {
+						request: testCase.request,
+						expectation: testCase.expectation
+					}
+				})
+			}
+		}
+	}
+}
+
+function assertResults(suite, results, options) {
+	let exitError = false;
+	const description = (index) => suite.testCases[index].description || '';
+
+	log(
+		console.log,
+		suite.description,
+		options.logging && suite.description
+	);
+
+	results.map((result, index) => {
+		if (result.state === "SUCCESS") {
+			log(
+				console.log,
+				`\t${index}. ${clc.green("[SUCCESS]")} ${description(index)}`,
+				options.logging
+			);
+		} else {
+			log(
+				console.error,
+				clc.red(`\t${index}. [FAILURE] ${description(index)}`),
+				options.logging
+			)
+			exitError = true;
+		}
+	});
+
+	if (exitError) {
+		log(
+			console.error,
+			'Some tests failed',
+			options.logging
+		);
+		process.exit(1);
+	}
+}
+
+function validateRuleSuite(suite, options) {
+	return requireAuth({}).then(() =>
+		api.request(
+			"POST",
+			`/v1/projects/${suite.project}:test`,
+			buildRequest(suite)
+		)).then(res => {
+			return res.body.testResults;
+		}).then(results =>
+			assertResults(suite, results, options))
+		.catch(err => {
+			log(console.error, clc.red(err.message), true);
+		});
+}
+
+
+function TestCase(description, options) {
+	this.request = {
+		...options,
+		auth: options.auth || null,
+		time: options.time || new Date().toISOString(),
+	}
+
+	console.log(this.request);
+
+	this.description = description;
+
+	this.shouldFail = () => {
+		this.expectation = "DENY";
+	}
+
+	this.shouldSucced = () => {
+		this.expectation = "ALLOW";
+	}
+
+	return this;
+}
+
+function RuleTestSuite({ project, rulePath, description }) {
+	this.testCases = [];
+	this.ruleName = rulePath;
+	this.ruleContent = fs.readFileSync(rulePath).toString();
+	this.project = project;
+	this.description = description;
+
+	this.test = function (description, options) {
+		var testCase = new TestCase(description, options);
+		this.testCases.push(testCase);
+		return testCase;
+	}
+
+	return this;
+}
+
+module.exports = {
+	validateRuleSuite,
+	RuleTestSuite
+};
